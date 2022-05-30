2731d62553ed334ea880c64f5fa9374a1d65bc9d
Alice Rossi <naanna275@gmail.com>
2022-05-30 16:01:52 +0300

    beautified currency check

--- page_objects/main_page.py
+++ page_objects/main_page.py
@@ -22,16 +22,25 @@ class MainPage(BasePage):
         self._click(CurrencyLocators.CURRENCY_BUTTON)
         self._click(CurrencyLocators.POUND_BUTTON)
 
-    # проверки валюты в корзине и верхнем валютном меню
 
-    def check_euro_currency_menu_and_cart(self):
-        self._verify_element_presence(CurrencyLocators.ACTIVE_CURRENCY_EURO)
-        self._verify_element_presence(CurrencyLocators.EURO_CURRENCY_IN_CART)
-
-    def check_dollar_currency_menu_and_cart(self):
-        self._verify_element_presence(CurrencyLocators.ACTIVE_CURRENCY_DOLLAR)
-        self._verify_element_presence(CurrencyLocators.DOLLAR_CURRENCY_IN_CART)
-
-    def check_pound_currency_menu_and_cart(self):
-        self._verify_element_presence(CurrencyLocators.ACTIVE_CURRENCY_POUND)
-        self._verify_element_presence(CurrencyLocators.POUND_CURRENCY_IN_CART)
+    def check_currency_menu_and_cart(self, currency):
+        currency_mapper = {
+            "EURO": {
+                "active_currency_list": CurrencyLocators.ACTIVE_CURRENCY_EURO,
+                "currency_in_cart": CurrencyLocators.EURO_CURRENCY_IN_CART,
+                "button_clicker": self.click_euro_currency_button
+            },
+            "DOLLAR": {
+                "active_currency_list": CurrencyLocators.ACTIVE_CURRENCY_DOLLAR,
+                "currency_in_cart": CurrencyLocators.DOLLAR_CURRENCY_IN_CART,
+                "button_clicker": self.click_dollar_currency_button
+            },
+            "POUND": {
+                "active_currency_list": CurrencyLocators.ACTIVE_CURRENCY_POUND,
+                "currency_in_cart": CurrencyLocators.POUND_CURRENCY_IN_CART,
+                "button_clicker": self.click_pound_currency_button
+            }
+        }
+        currency_mapper.get(currency).get("button_clicker")()
+        self._verify_element_presence(currency_mapper.get(currency).get("active_currency_list"))
+        self._verify_element_presence(currency_mapper.get(currency).get("currency_in_cart"))
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,16 +1,10 @@
+import pytest
+
 from page_objects.main_page import MainPage
 
 
-def test_currency(browser):
+@pytest.mark.parametrize('currency', ['DOLLAR', 'EURO', 'POUND'])
+def test_currency(browser, currency):
     main_page = MainPage(browser)
 
-    main_page.click_euro_currency_button()
-    main_page.check_euro_currency_menu_and_cart()
-
-    main_page.click_dollar_currency_button()
-    main_page.check_dollar_currency_menu_and_cart()
-
-    main_page.click_pound_currency_button()
-    main_page.check_pound_currency_menu_and_cart()
-
-
+    main_page.check_currency_menu_and_cart(currency)
