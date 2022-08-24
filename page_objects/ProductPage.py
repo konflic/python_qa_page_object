a13621f565667602c4e832fddaf7e6cbe3317254
konflic <konflic@mail.ru>
2022-08-25 00:02:58 +0300

    Продолжаем выносить методы в классы

--- page_objects/ProductPage.py
+++ page_objects/ProductPage.py
@@ -1,2 +1,18 @@
+import time
+from selenium.webdriver.common.by import By
+
+
 class ProductPage:
-    pass
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def add_to_wish_list(self):
+        self.driver.find_element(By.CSS_SELECTOR, "[data-original-title='Add to Wish List']").click()
+
+    def add_to_comparison(self):
+        self.driver.find_element(By.CSS_SELECTOR, "[data-original-title='Compare this Product']").click()
+
+    def add_to_cart(self):
+        time.sleep(1.5)  # Page loading problem
+        self.driver.find_element_by_css_selector("#button-cart").click()
--- tests/test_example.py
+++ tests/test_example.py
@@ -6,13 +6,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from page_objects.MainPage import MainPage
 from page_objects.UserPage import UserPage
+from page_objects.ProductPage import ProductPage
 
 
 def test_add_to_wish_list(browser):
     # Выбор рекомендуемого продукта и сохранение названия
     product_name = MainPage(browser).click_featured_product(0)
     # Добавляем товар в вишлист
-    browser.find_element_by_css_selector("[data-original-title='Add to Wish List']").click()
+    ProductPage(browser).add_to_wish_list()
     # Клик по ссылке login корзины в алерте
     browser.find_element_by_css_selector(".alert-success").find_element_by_link_text('login').click()
     # Авторизация
@@ -27,8 +28,7 @@ def test_add_to_cart(browser):
     # Выбор рекомендуемого продукта и сохранение названия
     product_name = MainPage(browser).click_featured_product(1)
     # Клик по кнопке добавления в корзину
-    time.sleep(1.5) # Page loading problem
-    browser.find_element_by_css_selector("#button-cart").click()
+    ProductPage(browser).add_to_cart()
     # Клик по ссылке корзины в алерте
     browser.find_element_by_css_selector(".alert-success").find_element_by_link_text("shopping cart").click()
     # Ждём пока появится раздел с товаром
@@ -45,7 +45,7 @@ def test_add_to_cart_from_comparison(browser):
     # Выбор рекомендуемого продукта и сохранение названия
     product_name = MainPage(browser).click_featured_product(0)
     # Добавляем продукт в сравнение
-    browser.find_element_by_css_selector("[data-original-title='Compare this Product']").click()
+    ProductPage(browser).add_to_comparison()
     # Клик по ссылке сравнения в алерте
     browser.find_element_by_css_selector(".alert-success").find_element_by_link_text("product comparison").click()
     # Ждём пока появится раздел с товаром
