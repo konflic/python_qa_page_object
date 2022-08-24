9b316b590efdabc97e3d96d31c7b15ef2bbcb27a
konflic <konflic@mail.ru>
2022-08-25 00:24:39 +0300

    Завершаем паттерн в тестах

--- page_objects/elements/AlertElement.py
+++ page_objects/elements/AlertElement.py
@@ -1,2 +1,19 @@
+from selenium.webdriver.common.by import By
+
+
 class AlertElement:
-    pass
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    @property
+    def comparison(self):
+        return self.driver.find_element(By.CSS_SELECTOR, ".alert-success").find_element(By.LINK_TEXT, "product comparison")
+
+    @property
+    def login(self):
+        return self.driver.find_element(By.CSS_SELECTOR, ".alert-success").find_element(By.LINK_TEXT, 'login')
+
+    @property
+    def cart(self):
+        return self.driver.find_element(By.CSS_SELECTOR, ".alert-success").find_element(By.LINK_TEXT, "shopping cart")
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,66 +1,38 @@
-import time
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from page_objects.MainPage import MainPage
 from page_objects.UserPage import UserPage
 from page_objects.ProductPage import ProductPage
 from page_objects.CartPage import CartPage
 from page_objects.ComparisonPage import ComparisonPage
+from page_objects.elements.AlertElement import AlertElement
 
 
 def test_add_to_wish_list(browser):
-    # Выбор рекомендуемого продукта и сохранение названия
     product_name = MainPage(browser).click_featured_product(0)
-    # Добавляем товар в вишлист
     ProductPage(browser).add_to_wish_list()
-    # Клик по ссылке login корзины в алерте
-    browser.find_element_by_css_selector(".alert-success").find_element_by_link_text('login').click()
-    # Авторизация
+    AlertElement(browser).login.click()
     UserPage(browser).login("test2@mail.ru", "test")
-    # Переходим в вишлист пользователя
     UserPage(browser).open_wish_list()
-    # Ждём пока появится раздел с товаром
     UserPage(browser).verify_product_item(product_name)
 
 
 def test_add_to_cart(browser):
-    # Выбор рекомендуемого продукта и сохранение названия
     product_name = MainPage(browser).click_featured_product(1)
-    # Клик по кнопке добавления в корзину
     ProductPage(browser).add_to_cart()
-    # Клик по ссылке корзины в алерте
-    browser.find_element_by_css_selector(".alert-success").find_element_by_link_text("shopping cart").click()
-    # Ждём пока появится раздел с товаром
+    AlertElement(browser).cart.click()
     CartPage(browser).verify_product_item(product_name)
-    # Клик по кнопке checkout
     CartPage(browser).click_checkout()
-    # Авторизация
     UserPage(browser).login("test2@mail.ru", "test")
-    # Проверка появления формы оплаты
     UserPage(browser).verify_payment_form()
 
 
 def test_add_to_cart_from_comparison(browser):
-    # Выбор рекомендуемого продукта и сохранение названия
     product_name = MainPage(browser).click_featured_product(0)
-    # Добавляем продукт в сравнение
     ProductPage(browser).add_to_comparison()
-    # Клик по ссылке сравнения в алерте
-    browser.find_element_by_css_selector(".alert-success").find_element_by_link_text("product comparison").click()
-    # Ждём пока появится раздел с товаром
+    AlertElement(browser).comparison.click()
     ComparisonPage(browser).verify_product_item(product_name)
-    # Добавляем товар в корзину
     ComparisonPage(browser).add_to_cart()
-    # Клик по ссылке корзины в алерте
-    browser.find_element_by_css_selector(".alert-success").find_element_by_link_text("shopping cart").click()
-    # Ждём пока появится раздел с товаром
-    WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.LINK_TEXT, product_name)))
-    # Клик по кнопке checkout
+    AlertElement(browser).cart.click()
+    CartPage(browser).verify_product_item(product_name)
     CartPage(browser).click_checkout()
-    # Авторизация
     UserPage(browser).login("test2@mail.ru", "test")
-    # Проверка появления платёжной формы
     UserPage(browser).verify_payment_form()
