3a45af11430284f77722e793b888515d6f918fb6
konflic <konflic@mail.ru>
2023-12-24 14:03:41 +0300

    Продолжаем перенос методов

--- page_objects/product_page.py
+++ page_objects/product_page.py
@@ -1,2 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
 class ProductPage:
-    pass
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def add_to_cart(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#button-cart").click()
+
+    def add_to_comparison(self):
+        self.browser.find_element(By.CSS_SELECTOR, "[title='Compare this Product']").click()
+
+    def add_to_wish_list(self):
+        self.browser.find_element(By.CSS_SELECTOR, "[title='Add to Wish List']").click()
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,18 +1,17 @@
-import time
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from page_objects.main_page import MainPage
 from page_objects.user_page import UserPage
+from page_objects.product_page import ProductPage
 
 
 def test_add_to_wish_list(browser):
     # Выбор продукта из блока featured
     product_name = MainPage(browser).click_featured_product()
     # Работа на странице товара
-    browser.find_element_by_css_selector("[title='Add to Wish List']").click()
+    ProductPage(browser).add_to_wish_list()
     # Работа с алертом
     browser.find_element_by_css_selector(".alert-success").find_element_by_link_text('login').click()
     # Авторизация
@@ -27,7 +26,7 @@ def test_add_to_cart(browser):
     # Выбор продукта из блока featured
     product_name = MainPage(browser).click_featured_product()
     # Работа на странице товара
-    browser.find_element_by_css_selector("#button-cart").click()
+    ProductPage(browser).add_to_cart()
     # Работа с алертом
     browser.find_element_by_css_selector(".alert-success").find_element_by_link_text("shopping cart").click()
     # Работа в корзине
@@ -45,7 +44,7 @@ def test_add_to_cart_from_comparison(browser):
     # Выбор продукта из блока featured
     product_name = MainPage(browser).click_featured_product()
     # Работа на странице товара
-    browser.find_element_by_css_selector("[title='Compare this Product']").click()
+    ProductPage(browser).add_to_comparison()
     # Работа с алертом
     browser.find_element_by_css_selector(".alert-success").find_element_by_link_text("product comparison").click()
     # Работа на странице сравнения
